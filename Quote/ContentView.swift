4e203005b2297d638720e2ce61a96715e0af5335
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2024-06-27 10:30:22 +0800

    Initial Commit

---
+++ Quote/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Quote
+//
+//  Created by Runhua Huang on 2024/6/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Quote/QuoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteApp.swift
+//  Quote
+//
+//  Created by Runhua Huang on 2024/6/27.
+//
+
+import SwiftUI
+
+@main
+struct QuoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
